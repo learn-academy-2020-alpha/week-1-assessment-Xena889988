acce76cc7ff9b721ddc6838686a864b985ea9ddf
Ubuntu <ubuntu@ip-172-31-37-226.us-east-2.compute.internal>
2020-02-10 17:24:38 +0000

    submitting assessment

--- js-intro.js
+++ js-intro.js
@@ -1,61 +1,106 @@
-// ASSESSMENT 1: INTRO TO JAVASCRIPT
-// Coding practical questions
+// // ASSESSMENT 1: INTRO TO JAVASCRIPT
+// // Coding practical questions
 
-// Please read all questions thoroughly
-// Psuedo coding is highly recommended
+// // Please read all questions thoroughly
+// // Psuedo coding is highly recommended
 
-// --------------------1) Create a function that determines if a temperature is below boiling point, at boiling point or above boiling point. Boiling point is 212 degrees Farhenheit.
-// Use the test variables provided below. Expected outcome: "35 is below boiling point", "350 is above boiling point", "212 is at boiling point"
+// // --------------------1) Create a function that determines if a temperature is below boiling point, at boiling point or above boiling point. Boiling point is 212 degrees Farhenheit.
+// // Use the test variables provided below. Expected outcome: "35 is below boiling point", "350 is above boiling point", "212 is at boiling point"
 
-var temp1 = 35
-var temp2 = 350
-var temp3 = 212
+// var temp1 = 35
+// var temp2 = 350
+// var temp3 = 212
 
-
-
-
-// --------------------2) Create a function that multiplies each number in the array by 5 using a for loop.
-// Use the test variable provided below. Expected outcome: [15, 35, 0, 30, -45]
-
-var myNumbers1 = [3, 7, 0, 6, -9]
-
+// // if statement for testing temperrature below boiling point
+//     if (temp1 < temp3) {console.log (`${temp1} is below boiling point`);} 
+// // if statement for testing temperrature above boiling point    
+//     if (temp2 > temp3) {console.log (`${temp2} is above boiling point`);}
+// // if statement for testing temperrature at a boiling point
+//     if (temp3 == 212) {console.log (`${temp3} is at a boiling point`)}
 
 
 
-// --------------------3) Create a function that multiplies each number in the array by 5 using map.
-// Use the test variable provided below. Expected outcome: [40, -35, 0, 30, 10]
+// // --------------------2) Create a function that multiplies each number in the array by 5 using a for loop.
+// // Use the test variable provided below. Expected outcome: [15, 35, 0, 30, -45]
 
-var myNumbers2 = [8, -7, 0, 6, 2]
+// var myNumbers1 = [3, 7, 0, 6, -9]
+
+// for(let i=0; i<myNumbers1.length; i++){
+//     console.log(myNumbers1[i] * 5);
+// }
+
+// // --------------------3) Create a function that multiplies each number in the array by 5 using map.
+// // Use the test variable provided below. Expected outcome: [40, -35, 0, 30, 10]
+
+// var myNumbers2 = [8, -7, 0, 6, 2]
+
+// mult5 = (array) => {
+//     let newArr = array.map(value => value * 5 )
+//     return newArr
+// }
+// console.log(mult5(myNumbers2))
 
 
+// // --------------------4) Create a function that removes all the vowels from a string.
+// // Use the test variables provided below. Expected output: "HyThrLrnStdnt" "LvJvScrpt"
+
+// var stringWithVowels1 = function (HeyThereLearnStudent) {
+// var stringWithVowels2 = "ILoveJavaScript"
+// var stringWithVowels3 = stringWithVowels1.concat(stringWithVowels2);
+    
 
 
-// --------------------4) Create a function that removes all the vowels from a string.
-// Use the test variables provided below. Expected output: "HyThrLrnStdnt" "LvJvScrpt"
+// ONE WAY TO DO THIS
+// stringWithVowels3 =  stringWithVowels3.replace( /[aeiou]/g, '' );
 
-var stringWithVowels1 = "HeyThereLearnStudent"
-var stringWithVowels2 = "ILoveJavaScript"
+// console.log(stringWithVowels3);
 
+// ANOTHER WAY OF DOING IT
+// function disemvowel(stringWithVowels3) {
+//   var vowels = ['a', 'e', 'i', 'o', 'u'];
+  
+//   return stringWithVowels3.split('').filter(function(el) {
+//     return vowels.indexOf(el.toLowerCase()) == -1;
+//   }).join('');
+// }
 
-
-
+// console.log(disemvowel(stringWithVowels3));
 
 // --------------------5) Expand the logic from #4 to inform your user if the variable passed into the vowel removal function is not a string.
 // Copy and paste or rewrite the function. Use the test variables provided below.
 
-var notAString1 = true
-var notAString2 = 42
+// var notAString1 = true
+// var notAString2 = 42
+
+// have no idea what i am doing
+// if (typeof index === "number")
+//     alert("Varaible is undefined");
+//     else alert("Variable is defined"); 
+
+// var index = 2; 
+//     var result = (typeof index === 'number');
+//     alert(result);
 
 
 
 
-// --------------------6) Create a function that takes the toonimals array and returns an array with only the toon objects that are cats.
+// // --------------------6) Create a function that takes the toonimals array and returns an array with only the toon objects that are cats.
 // Expected output: [ { name: "Stimpy", animal: "cat" }, { name: "Scratchy", animal: "cat" }, { name: "Felix", animal: "cat" } ]
 
 
 var toonimals = [ {name: "Itchy", animal: "mouse"}, {name: "Stimpy", animal: "cat"}, {name: "Daffy", animal: "duck"}, {name: "Scratchy", animal: "cat"}, {name: "Ren", animal: "dog"}, {name: "Felix", animal: "cat"}]
+const onlyCat = (array) => {
+  let newArr = []
+  for(let i=0; i<array.length; i++){
+    if(typeof === "cat"){
+      newArr.push(array[i])
+    }
+  }
+  return newArr
+}
+
+console.log(onlyCat(toonimals))
 
 
-
-// --------------------7) Using the toonimals variable from #6, create a function that returns only the names of the non-cats.
-// Expected output: "Itchy" "Daffy" "Ren"
+// // --------------------7) Using the toonimals variable from #6, create a function that returns only the names of the non-cats.
+// // Expected output: "Itchy" "Daffy" "Ren"
--- js-testing.test.js
+++ js-testing.test.js
@@ -11,20 +11,48 @@
 
 // --------------------1a) Create a test for a function called addThemUp that takes two numbers as arguments and returns the sum.
 
-
-
+// describe("addThemUp", () => {
+//   test("it should sum two numbers correctly", () => {
+//     const sum = 1 + 2;
+//     const expectedResult = 4;
+//     expect(sum).toEqual(expectedResult);
+//   })
+// });
 
 
 // --------------------1b) See the test fail. THEN write the code to make the test pass.
 
 
-
+// describe("addThemUp", () => {
+//   test("it should sum two numbers correctly", () => {
+//     const sum = 1 + 2;
+//     const expectedResult = 3;
+//     expect(sum).toEqual(expectedResult);
+//   })
+// });
 
 
 // --------------------2a) Create a test for a function called triangleArea that takes the base and the height of a triange and returns the area. The area of a triange is the base times the height divided by two.
 
 
-
+describe("triangleArea", () => {
+  test("it should show the area of a triangle", () => {
+      let a = 3
+      let b = 4
+    const area = a * b / 2;
+    const expectedResult = area; 
+  })
+});
 
 
 // --------------------2b) See the test fail. THEN write the code to make the test pass.
+
+// describe("triangleArea", () => {
+//   test("it should show the area of a triangle", () => {
+//       let a = 3
+//       let b = 4
+//     const area = a * b / 2;
+//     const expectedResult = area;
+//     expect(area).toEqual(expectedResult);
+//   })
+// });
